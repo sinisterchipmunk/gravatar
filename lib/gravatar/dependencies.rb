90d7b93988c48557a26307ce352648b8521205bd
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2010-06-16 14:49:38 -0400

    Resolved: "uninitialized constant ActiveSupport::Cache" when used outside of a Rails project.

--- lib/gravatar/dependencies.rb
+++ lib/gravatar/dependencies.rb
@@ -2,11 +2,18 @@ if defined?(Rails)
   Rails.configuration.gem "sc-core-ext", ">= 1.2.0"
 elsif !defined?(Gem)
   require 'rubygems'
+  gem 'activesupport', '>= 2.3.5'
   gem 'sc-core-ext', '>= 1.2.0'
 end
 
-unless defined?(ScCoreExt) || defined?(Rails) # because Rails will load it later and we don't really need it quite yet.
-  require 'sc-core-ext'
+unless defined?(Rails) # because Rails will load it later and we don't really need it quite yet.
+  unless defined?(ActiveSupport::Cache)
+    require 'active_support'
+  end
+
+  unless defined?(ScCoreExt)
+    require 'sc-core-ext'
+  end
 end
 
 # The rest of this is core Ruby stuff so it's safe to load immediately, even if Rails is running the show.
