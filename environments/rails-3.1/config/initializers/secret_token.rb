8b3f162861e3afca7f122bad1b24515bd02acccc
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-01-12 14:52:34 -0500

    Use 1.8 to generate rails3 project, so hashes are compatible

--- environments/rails-3.1/config/initializers/secret_token.rb
+++ environments/rails-3.1/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails31::Application.config.secret_token = '3680e52eb2d3d9e65643ab3e63b15e88d086e906d60c33e7bdda24bd31f86ce079e9f71c8dd0a6fd0526ec4a82aa9e9b7b6adf7eb4c3bf1b7bbeca55fa7ef680'
+Rails31::Application.config.secret_token = '35910e6c8c4bebeb4b8123af88072bbe9b3ffcccd7aa577bfddb495cd95842ecdcbbb8bb2c9c43af11dafa0b14e848ae9b0ba044686f0005baeab3fb1b34c4d5'
