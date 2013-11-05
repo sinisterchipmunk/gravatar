0268911d688a294fc114c9f07e0b5f84d60b546e
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2013-11-05 01:31:09 -0500

    delete obsolete testbed
    not supporting rails 2 any more.

--- environments/rails-2.3/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f2d5d667c3cb44da4c52ccc91b0d6a64348a97ca8cc6fa3e401eb752c053e1c09a12ebd75f5390eb02699f120d64761f34f6cf42be027259374cc5e98d410060';
